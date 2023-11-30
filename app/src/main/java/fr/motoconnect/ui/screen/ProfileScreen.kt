e351e8896d7a25600162ecd1c8633c5e5ea0de20
Whilsterr <matteomonteserin@gmail.com>
2023-11-30 14:23:39 +0100

    Prototype User Card

--- app/src/main/java/fr/motoconnect/ui/screen/ProfileScreen.kt
+++ app/src/main/java/fr/motoconnect/ui/screen/ProfileScreen.kt
@@ -1,19 +1,27 @@
 package fr.motoconnect.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import fr.motoconnect.R
 import fr.motoconnect.data.model.UserData
 
 
@@ -23,18 +31,42 @@ fun ProfileScreen(
     onSignOut: () -> Unit
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+            .padding(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
+        //verticalArrangement = Arrangement.Center
     ) {
         if(userData?.username != null) {
-            Text(
-                text = userData.username,
-                textAlign = TextAlign.Center,
-                fontSize = 36.sp,
-                fontWeight = FontWeight.SemiBold
-            )
-            Text(text = userData.userId)
+            Card (
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ),modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth())
+            {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = painterResource(R.drawable.moto),
+                        contentDescription = "Image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.padding(14.dp)
+                    )
+                    Column(
+                        modifier = Modifier.padding(14.dp),
+                        verticalArrangement = Arrangement.Bottom
+                    ) {
+                        Text(
+                            text = userData.username,
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.SemiBold,
+                        )
+                        Text(
+                            text = userData.userId,
+                            fontSize = 15.sp,
+                            )
+                    }
+                }
+            }
             Spacer(modifier = Modifier.height(16.dp))
         }
         Button(onClick = onSignOut) {
