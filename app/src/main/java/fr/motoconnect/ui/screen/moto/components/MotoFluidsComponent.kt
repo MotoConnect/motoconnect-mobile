a2de42303c4013a181343d0cbf78487e20e2a8a9
Whilsterr <matteomonteserin@gmail.com>
2024-02-14 16:54:52 +0100

    Initial commit, refactoring MotoFluidsComponent

--- app/src/main/java/fr/motoconnect/ui/screen/moto/components/MotoFluidsComponent.kt
+++ app/src/main/java/fr/motoconnect/ui/screen/moto/components/MotoFluidsComponent.kt
@@ -2,21 +2,23 @@ package fr.motoconnect.ui.screen.moto.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -24,114 +26,200 @@ import androidx.compose.ui.unit.sp
 import fr.motoconnect.R
 import fr.motoconnect.data.model.BaseDistance
 import fr.motoconnect.viewmodel.uiState.MotoUIState
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun MotoFluidsComponent(
     motoUIState: MotoUIState,
     resetEngineOil: () -> Unit,
-    resetBreakFluid: () -> Unit,
+    resetBrakeFluid: () -> Unit,
     resetChainLubrication: () -> Unit,
 ) {
-    Box(
+
+    val engineOilPercentage =
+        motoUIState.moto?.engineOil?.toFloat()?.div(BaseDistance.ENGINE_OIL.distance.toFloat())
+            ?: 0f
+    val breakFluidPercentage =
+        motoUIState.moto?.breakFluid?.toFloat()?.div(BaseDistance.BRAKE_FLUID.distance.toFloat())
+            ?: 0f
+    val chainLubricationPercentage = motoUIState.moto?.chainLubrication?.toFloat()
+        ?.div(BaseDistance.CHAIN_LUBRICATION.distance.toFloat()) ?: 0f
+
+    LazyColumn(
         modifier = Modifier
-            .padding(0.dp, 16.dp)
-            .clip(RoundedCornerShape(10))
+            .fillMaxWidth()
             .background(MaterialTheme.colorScheme.primary)
-            .padding(16.dp)
-            .fillMaxWidth(),
+            .padding(16.dp, 0.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        Column {
-            Text(
-                text = stringResource(R.string.moto_maintenance),
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.tertiary,
-                modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp),
-            )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+        item {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+        item {
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.onPrimary,
+                    contentColor = MaterialTheme.colorScheme.tertiary
+                ),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ), modifier = Modifier
+                    .fillMaxWidth()
             ) {
-                Text(
-                    text = stringResource(R.string.moto_engine_oil),
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colorScheme.tertiary,
-                    modifier = Modifier.width(120.dp)
-                )
-                Text(
-                    text = "${motoUIState.moto?.engineOil}/${BaseDistance.ENGINE_OIL.distance} km",
-                    color = MaterialTheme.colorScheme.tertiary,
-                    fontSize = 16.sp,
-                )
-                Button(
-                    onClick = { resetEngineOil() },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.tertiary,
-                        contentColor = MaterialTheme.colorScheme.primary
-                    )
-                ){
-                    Text(text = stringResource(R.string.moto_reset))
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth().padding(15.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+
+                    ) {
+                        Column {
+                            Text(
+                                text = stringResource(R.string.moto_engine_oil),
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.tertiary,
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            LinearProgressIndicator(
+                                progress = engineOilPercentage,
+                                color = MaterialTheme.colorScheme.secondary,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceBetween) {
+                                Button(
+                                    onClick = { resetEngineOil() },
+                                    colors = ButtonDefaults.buttonColors(
+                                        containerColor = MaterialTheme.colorScheme.tertiary,
+                                        contentColor = MaterialTheme.colorScheme.primary
+                                    )
+                                ) {
+                                    Text(text = stringResource(R.string.moto_reset))
+                                }
+                                Text(
+                                    text = "${motoUIState.moto?.engineOil}/${BaseDistance.ENGINE_OIL.distance} km",
+                                    color = MaterialTheme.colorScheme.tertiary,
+                                    fontSize = 20.sp,
+                                    textAlign = TextAlign.End,
+                                    modifier= Modifier.padding(0.dp, 10.dp, 0.dp, 0.dp)
+                                )
+                            }
+
+                        }
+                    }
                 }
             }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = stringResource(R.string.moto_break_fluid),
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colorScheme.tertiary,
-                    modifier = Modifier.width(120.dp)
-                )
-                Text(
-                    text = "${motoUIState.moto?.breakFluid}/${BaseDistance.BREAK_FLUID.distance} km",
-                    fontSize = 16.sp,
-                    color = MaterialTheme.colorScheme.tertiary,
-                )
-                Button(
-                    onClick = { resetBreakFluid() },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.tertiary,
-                        contentColor = MaterialTheme.colorScheme.primary
-                    )
-                ){
-                    Text(text = stringResource(R.string.moto_reset))
+        }
+        item {
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.onPrimary,
+                    contentColor = MaterialTheme.colorScheme.tertiary
+                ),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ), modifier = Modifier
+                    .fillMaxWidth()
+            ){
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth().padding(15.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column (modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = stringResource(R.string.moto_break_fluid),
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.tertiary,
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        LinearProgressIndicator(
+                            progress = breakFluidPercentage,
+                            color = MaterialTheme.colorScheme.secondary,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceBetween) {
+                            Button(
+                                onClick = { resetBrakeFluid() },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = MaterialTheme.colorScheme.tertiary,
+                                    contentColor = MaterialTheme.colorScheme.primary
+                                ),
+                            ) {
+                                Text(text = stringResource(R.string.moto_reset))
+                            }
+                            Text(
+                                text = "${motoUIState.moto?.breakFluid}/${BaseDistance.BRAKE_FLUID.distance} km",
+                                color = MaterialTheme.colorScheme.tertiary,
+                                fontSize = 20.sp,
+                                textAlign = TextAlign.End,
+                                modifier= Modifier.padding(0.dp, 10.dp, 0.dp, 0.dp)
+                            )
+                        }
+                    }
                 }
             }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = stringResource(R.string.moto_chain_greasing),
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colorScheme.tertiary,
-                    modifier = Modifier.width(120.dp)
-                )
-                Text(
-                    text = "${motoUIState.moto?.chainLubrication}/${BaseDistance.CHAIN_LUBRICATION.distance} km",
-                    fontSize = 16.sp,
-                    color = MaterialTheme.colorScheme.tertiary,
-                )
-                Button(
-                    onClick = { resetChainLubrication() },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.tertiary,
-                        contentColor = MaterialTheme.colorScheme.primary
-                    )
-                ){
-                    Text(text = stringResource(R.string.moto_reset))
+        }
+        item {
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.onPrimary,
+                    contentColor = MaterialTheme.colorScheme.tertiary
+                ),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ), modifier = Modifier
+                    .fillMaxWidth()
+            ){
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth().padding(15.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column (modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = stringResource(R.string.moto_chain_greasing),
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.tertiary
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        LinearProgressIndicator(
+                            progress = chainLubricationPercentage,
+                            color = MaterialTheme.colorScheme.secondary,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceBetween) {
+                            Button(
+                                onClick = { resetChainLubrication() },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = MaterialTheme.colorScheme.tertiary,
+                                    contentColor = MaterialTheme.colorScheme.primary
+                                )
+                            ) {
+                                Text(text = stringResource(R.string.moto_reset))
+                            }
+                            Text(
+                                text = "${motoUIState.moto?.chainLubrication}/${BaseDistance.CHAIN_LUBRICATION.distance} km",
+                                color = MaterialTheme.colorScheme.tertiary,
+                                fontSize = 20.sp,
+                                textAlign = TextAlign.End,
+                                modifier= Modifier.padding(0.dp, 10.dp, 0.dp, 0.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
+
